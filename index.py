9391ccb7f180fcd569b0996e6d5e54d7007f4f31
wbt <2949588484@qq.com>
2020-02-07 00:53:29 +0800

    first version

---
+++ index.py
@@ -0,0 +1,90 @@
+#coding=utf-8
+import eel
+import json
+import requests
+from bs4 import BeautifulSoup
+
+eel.init('web')
+
+# 爬取微博热搜
+@eel.expose
+def weibo_hot_search():
+	weibo_url= "https://s.weibo.com/top/summary?cate=realtimehot" 
+	html= requests.get(weibo_url)
+
+	soup= BeautifulSoup(html.text, "html.parser")
+	table_hot= soup.find("div", id= "pl_top_realtimehot").find("table")
+
+	# 热搜和热搜链接
+	links= table_hot.find_all('a')
+	link_text= []
+	for top in links:
+		link_text.append(top['href'])
+		link_text.append(top.text)
+	return link_text
+
+@eel.expose
+def background_img():
+	url_api= "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN"
+	json_str= requests.get(url_api)
+	json_res= json.loads(json_str.text)
+
+	url_bing= "https://cn.bing.com"
+	url_img= url_bing+ json_res["images"][0]["url"]
+	# print(url_img)
+	return url_img
+
+def get_location():
+	#还可使用 http://ip.42.pl/raw、https://jsonip.com/、https://api.ipify.org/?format=json、http://httpbin.org/ip
+	# https://ipv4.jsonip.com
+	# url_api_getip= "https://jsonip.com/"
+	# ip= json.loads(requests.get(url_api_getip).text)
+
+	# print(ip)
+	url_ip_api= "http://freeapi.ipip.net/"+ "120.231.18.210"  #ip["ip"]
+	res= requests.get(url_ip_api)
+	res= json.loads(res.text)
+	print(res[2])
+	return res[2]
+
+@eel.expose
+def weather():
+	# 区域adcode查询--高德api
+	url_api= "https://restapi.amap.com/v3/config/district?"
+	keywords= get_location()
+	key= "39d6421326c3113cc533e403191b833e"
+	subdistrict= "0"
+	extensions= "base"
+	url_api+= "key="+key+ "&keywords="+keywords+ "&subdistrict="+subdistrict+"&extensions="+ extensions
+	res= requests.get(url_api)
+	res= json.loads(res.text)
+	adcode= res["districts"][0]["adcode"]
+	print(res["districts"][0]["adcode"])
+
+	# 天气查询--高德api
+	url_api= "https://restapi.amap.com/v3/weather/weatherInfo?"
+	extensions= "all"
+	url_api+= "key="+key+ "&city="+ adcode+ "&extensions="+ extensions
+	res= requests.get(url_api)
+	res= json.loads(res.text)
+	today= res["forecasts"][0]["casts"][0]
+	print(str(today))
+	return str(today)
+
+	# 天气查询--openWeatherMap
+	# url_api= "http://api.openweathermap.org/data/2.5/weather?lang=zh_cn&units=metric"
+	# appid= "37127bd7576609e7bd24f267729cd24d"
+	# q= "zhanjiang"
+	# url_api+= "&q="+ q+ "&APPID="+ appid
+	# json_str= requests.get(url_api)
+
+	# # print(json_str.text)
+	# return json_str.text
+
+# eel.start("index.html", mode= 'edge', port= 8087)
+
+# python调试
+if __name__== '__main__':
+	# weibo_hot_search()
+	weather()
+	# background_img()
---
+++ web/js/index.js
@@ -0,0 +1,89 @@
+function getTime(){
+	// 更新时间
+	str = ""
+	var p = document.getElementById("time_now");
+	time =  new Date();
+	year = time.getFullYear();
+	month = time.getMonth() + 1;
+	day = time.getDate();
+	hour = time.getHours();
+	minutes = time.getMinutes();
+	seconds = time.getSeconds();
+	str = str + year +"-"+ month +"-"+ day + " " +hour+":"+minutes+":"+seconds;
+	
+	p.innerText = str;
+ 
+	setTimeout(getTime,1000);
+}
+
+function checkMouse(ele){
+	// 监听鼠标事件
+	var class_name= ele.className;
+	// alert(class_name)
+	if(class_name== "weibo_hot"){
+		var weibo_hot= document.getElementById("weibo_hot");
+		weibo_hot.style.display= weibo_hot.style.display=="block"?"none": "block";
+	}
+
+	if(class_name== "weather"){
+		var weibo_hot= document.getElementById("weather");
+		weibo_hot.style.display= weibo_hot.style.display=="block"?"none": "block";
+		var weibo_desc= document.getElementById("weather_desc");
+		weibo_desc.style.display= weibo_desc.style.display=="none"?"block": "none";
+	}
+}
+async function weather(){
+	// 利用openWeatherMap的weather api获取地址天气情况
+	var weather_res_str= await eel.weather()()
+	weather_res= JSON.parse(weather_res_str)
+
+	var name= weather_res["name"]
+	var temp_now= weather_res["main"]["temp"]+ "℃"
+	var feels_like= weather_res["main"]["feels_like"]+ "℃"
+	var temp_min= weather_res["main"]["temp_min"]+ "℃"
+	var temp_max= weather_res["main"]["temp_max"]+ "℃"
+	var humidity= weather_res["main"]["humidity"]+ "%"
+	var clouds= weather_res["clouds"]["all"]+ "%"
+
+	var weather_id= document.getElementById("weather");
+	weather_id.innerHTML= "<div><h4>"+ name+ "</h4>"
+		+"<h5>temp:"+ temp_now+ " feels_like:"+ feels_like+  " humidity:"+ humidity
+		+" temp_min/max"+ temp_min+ "/"+ temp_max+" clouds:"+ clouds+ "</h5></div>"
+
+	setTimeout(weather, 5*60*1000);
+}
+async function weibo_hot_search(){
+	// 利用爬虫爬取微博热搜
+	var weibo_url= "https://s.weibo.com";
+    var weibo_hot= document.getElementById("weibo_hot");
+	var hot_result= await eel.weibo_hot_search()();
+	
+	weibo_hot_str= "<table>";
+	for(var i= 0; i< hot_result.length; ){
+		if(i%6== 0)
+			weibo_hot_str+= "<tr>";
+		weibo_hot_str+= " <td><a href= '"
+		weibo_hot_str+= weibo_url+ hot_result[i++];
+		weibo_hot_str+= "' target='_blank'>";
+		weibo_hot_str+= parseInt(i/2) + "、"+ hot_result[i++];
+		weibo_hot_str+= "</a></td>";
+		if(i%6== 0)
+			weibo_hot_str+= " </tr>";
+	}
+	weibo_hot_str+= "</table>";
+	weibo_hot.innerHTML= weibo_hot_str;
+	setTimeout(weibo_hot_search, 60*1000);
+}
+async function change_background_img(){
+	// 利用bing每日一图更换背景图片
+	var body= document.getElementById("body")
+	var background_img= await eel.background_img()()
+
+	body.style.backgroundImage= "url('"+ background_img+ "')"
+}
+window.onload = function(){
+	this.change_background_img();
+	this.getTime();
+	this.weibo_hot_search();
+	this.weather();
+}
